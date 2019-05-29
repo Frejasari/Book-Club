d60f2248e8b0de4b02c30882ac885ddefc8e09ee
Sari <freja.sender@gmail.com>
2019-05-30 00:11:52 +0200

    add camera to take cover picture

--- app/src/main/java/de/frejasundalexchat/dritterchat/bookOverview/CreateBookFragment.kt
+++ app/src/main/java/de/frejasundalexchat/dritterchat/bookOverview/CreateBookFragment.kt
@@ -1,6 +1,11 @@
 package de.frejasundalexchat.dritterchat.bookOverview
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -9,30 +14,44 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.content.FileProvider
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputLayout
+import com.squareup.picasso.Picasso
 import de.frejasundalexchat.dritterchat.R
 import de.frejasundalexchat.dritterchat.db.ObjectBox
 import de.frejasundalexchat.dritterchat.error.ValidationError
 import de.frejasundalexchat.dritterchat.model.Book
 import io.objectbox.kotlin.boxFor
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 private const val TITLE = "book title"
-private const val COVERURL = "book cover url"
+private const val COVERURI = "book cover url"
 
 class CreateBookFragment : Fragment() {
+
+    private val REQUEST_IMAGE_CAPTURE = 19
+
     // TODO: Rename and change types of parameters
     private var title: String = ""
-    private var coverUrl: String = ""
+    private var coverUri: String = ""
 
     private lateinit var titleInput: EditText
-    private lateinit var coverUrlInput: EditText
     private lateinit var saveButton: Button
     private lateinit var abortButton: Button
+    private lateinit var selectPictureButton: View
+    private lateinit var coverImagePreview: ImageView
+    private lateinit var backArrow: View
 
     private val bookBox = ObjectBox.get().boxFor(Book::class)
+
+    private var currentPhotoPath: String? = null
 
     companion object {
         fun newInstance(): CreateBookFragment {
@@ -45,7 +64,7 @@ class CreateBookFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
             title = it.getString(TITLE) ?: ""
-            coverUrl = it.getString(COVERURL) ?: ""
+            coverUri = it.getString(COVERURI) ?: ""
         }
     }
 
@@ -59,44 +78,95 @@ class CreateBookFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         titleInput = view.findViewById(R.id.bookTitleInput)
-        coverUrlInput = view.findViewById(R.id.imgUrlInput)
         saveButton = view.findViewById(R.id.saveBookButton)
         abortButton = view.findViewById(R.id.abortButton)
+        selectPictureButton = view.findViewById(R.id.coverImagePreview)
+        coverImagePreview = view.findViewById(R.id.coverImagePreview)
+        backArrow = view.findViewById(R.id.backArrow)
 
+        selectPictureButton.setOnClickListener(this::onSelectPicture)
         abortButton.setOnClickListener { activity?.finish() }
+        backArrow.setOnClickListener { activity?.finish() }
         saveButton.setOnClickListener(this::onSave)
 
-
         title.apply { titleInput.setText(title) }
-        coverUrl.apply { coverUrlInput.setText(coverUrl) }
 
         titleInput.addOnTextChangedListener { text -> title = text }
-        coverUrlInput.addOnTextChangedListener { text -> coverUrl = text }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putString(TITLE, title)
-        outState.putString(COVERURL, coverUrl)
+        outState.putString(COVERURI, coverUri)
         super.onSaveInstanceState(outState)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val file = File(currentPhotoPath)
+            coverUri = Uri.fromFile(file).toString()
+            Picasso.get().load(coverUri).into(coverImagePreview)
+        }
     }
 
     private fun onSave(saveButton: View) {
         val errors = this.validateInput()
         if (errors.isEmpty()) {
-            bookBox.put(Book(0, title, coverUrl, 100))
+            bookBox.put(Book(0, title, coverUri, 100))
             activity?.finish()
         } else {
             errors.forEach { view!!.findViewById<TextInputLayout>(it.viewId).error = it.message }
         }
     }
 
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(activity!!.packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        context!!,
+                        "de.frejasundalexchat.dritterchat.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val storageDir: File = activity!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun onSelectPicture(button: View) {
+        dispatchTakePictureIntent()
+    }
+
     private fun validateInput(): List<ValidationError> {
         val errors = mutableListOf<ValidationError>()
         if (this.title.isBlank()) {
             errors.add(ValidationError(R.id.bookNameInputLayout, "Please set a title."))
-        }
-        if (this.coverUrl.isBlank()) {
-            errors.add(ValidationError(R.id.imgUrlInputLayout, "Please select a valid image.")) // TODO
         }
         return errors
     }
